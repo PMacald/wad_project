9729c19c9cc4d6d03006cc7be0466c528611f428
ScottWilson <33161958+ScottWilson@users.noreply.github.com>
2018-03-16 13:23:24 +0000

    scott: (jquery) posts tweaks, navbar now hides on scroll

--- wad_project/static/js/pethub-jquery.js
+++ wad_project/static/js/pethub-jquery.js
@@ -1,12 +1,26 @@
 $(document).ready(function() {
 	// Main
 	
-	// Test code - turns p elements red on hover, then blue
+	// Hide navbar on scroll
+	$(window).scroll(function() {
+		var i = $(window).scrollTop();
+		if (i >= 100) {
+			$("#navbar").fadeOut("fast");
+		} else {
+			$("#navbar").fadeIn("fast");
+		}
+	});
+	
+	
+	// Post container expand and light up on focus
 	$(".post_container").click(function() {
-		//$(this).hide();
-		
-		$(this).animate({left:'100px'});
-		$(this).css('borderColor', 'yellow');
+		// Deactivate previous post
+		$(".post_container").animate({width:"70%"}, "fast");
+		$(".post_container").css({'borderColor': 'purple', 'borderWidth' : '2px'});
+
+		// Expand post		
+		$(this).animate({width:"80%"});
+		$(this).css({'borderColor': 'yellow', 'borderWidth': '5px'});
 	});
 	
 });
