fd7d5956cdc7cc07362f9be76e1bb7b9799fd775
Lucy <2137998c@student.gla.ac.uk>
2018-03-16 09:03:23 +0000

    Added View Tests

--- wad_project/pethub/tests.py
+++ wad_project/pethub/tests.py
@@ -2,5 +2,48 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from .models import UserProfile, Post, Comment
+from django.core.urlresolvers import reverse
+from django.conf.urls import url
 
-# Create your tests here.
+class PetHubTests(TestCase):
+    
+    #View Tests
+    def test_cant_see_index_while_not_logged_in(self):
+        response = self.client.get(reverse('index'))
+        self.assertRedirects(response, '/pethub/login/?next=/pethub/index/')
+        
+    def test_cant_see_about_while_not_logged_in(self):
+        response = self.client.get(reverse('about-us'))
+        self.assertRedirects(response, '/pethub/login/?next=/pethub/about-us/')
+
+    def test_cant_see_trending_while_not_logged_in(self):
+        response = self.client.get(reverse('trending'))
+        self.assertRedirects(response, '/pethub/login/?next=/pethub/trending/')
+
+    def test_cant_see_extra_info_while_not_logged_in(self):
+        response = self.client.get(reverse('extra-information'))
+        self.assertRedirects(response, '/pethub/login/?next=/pethub/extra-information/')
+
+    def test_cant_see_species_while_not_logged_in(self):
+        response = self.client.get(reverse('species'))
+        self.assertRedirects(response, '/pethub/login/?next=/pethub/species/')
+
+    def test_cant_see_user_profile_while_not_logged_in(self):
+        response = self.client.get(reverse('user_profile'))
+        self.assertRedirects(response, '/pethub/login/?next=/pethub/user/')
+
+    def test_cant_see_post_upload_while_not_logged_in(self):
+        response = self.client.get(reverse('post-upload'))
+        self.assertRedirects(response, '/pethub/login/?next=/pethub/post-upload/')
+
+    def test_cant_see_user_logout_while_not_logged_in(self):
+        response = self.client.get(reverse('logout'))
+        self.assertRedirects(response, '/pethub/login/?next=/pethub/logout/')
+
+    def test_cant_see_update_user_while_not_logged_in(self):
+        response = self.client.get(reverse('update-user'))
+        self.assertRedirects(response, '/pethub/login/?next=/pethub/update-user/')
+
+    
+    
