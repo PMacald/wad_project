c0bf81174ba29fc180c7dcbec7117015d12b5645
Lucy <2137998c@student.gla.ac.uk>
2018-03-16 09:53:07 +0000

    Added Model Tests (1 Currently Fails)

--- wad_project/pethub/tests.py
+++ wad_project/pethub/tests.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.test import TestCase
 from .models import UserProfile, Post, Comment
 from django.core.urlresolvers import reverse
 from django.conf.urls import url
+import datetime
+from django.utils import timezone
 
 class PetHubTests(TestCase):
     
@@ -45,5 +46,22 @@ class PetHubTests(TestCase):
         response = self.client.get(reverse('update-user'))
         self.assertRedirects(response, '/pethub/login/?next=/pethub/update-user/')
 
-    
-    
+    #Model Tests
+    def test_ensure_2_post_with_same_title_can_exist(self):
+        test_post1 = Post(title="Test")
+        test_post2 = Post(title="Test")
+        self.assertEqual(test_post1==test_post2, False)
+
+    def test_post_cannot_have_negative_likes(self):
+        test_post = Post(likes=-1)
+        self.assertEqual((test_post.likes >= 0), True) #Currently this test fails, need to edit code elsewhere
+
+    def test_can_have_post_with_historic_date(self):
+        time = timezone.now() - datetime.timedelta(days=4)
+        test_post = Post(upload_date = time)
+        self.assertEqual((test_post.upload_date==timezone.now()), False)
+
+    def test_can_have_2_comments_with_same_text_on_post(self):
+        test_comment1 = Comment(comment_text = "Test")
+        test_comment2 = Comment(comment_text = "Test")
+        self.assertEqual(test_comment1==testcomment2, False)
