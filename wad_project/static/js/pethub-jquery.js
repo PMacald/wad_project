3434e83521a87356590b380550722a1d18612b92
ScottWilson <33161958+ScottWilson@users.noreply.github.com>
2018-03-16 10:41:41 +0000

    scott: fixed the post jquery hallelujah

--- wad_project/static/js/pethub-jquery.js
+++ wad_project/static/js/pethub-jquery.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
 	// Main
 	
-
 	// Test code - turns p elements red on hover, then blue
 	$(".post_container").click(function() {
-		$("div").animate({left: '100px'});
+		//$(this).hide();
+		
+		$(this).animate({left:'100px'});
+		$(this).css('borderColor', 'yellow');
 	});
 	
 });
