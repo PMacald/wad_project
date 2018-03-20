92a1f985e9c141922467286139fa151108c72b1f
PMacald <peter.i.macaldowie@gmail.com>
2018-03-20 11:33:46 +0000

    Likes now work on all pages

--- wad_project/static/js/pethub-jquery.js
+++ wad_project/static/js/pethub-jquery.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	// Main
-<<<<<<< HEAD
+
 
 	// Test code - turns p elements red on hover, then blue
 	$(".post_container").click(function() {
@@ -8,7 +8,7 @@ $(document).ready(function() {
 
 		$(this).animate({left:'100px'});
 		$(this).css('borderColor', 'yellow');
-=======
+
 
 	// Hide navbar on scroll
 	$(window).scroll(function() {
@@ -30,7 +30,7 @@ $(document).ready(function() {
 		// Expand post		
 		$(this).animate({width:"80%"});
 		$(this).css({'borderColor': 'yellow', 'borderWidth': '5px'});
->>>>>>> 4c27bcd42d17d6642b7c1ad9f47527dbf57a30e7
+
 	});
 
 });
