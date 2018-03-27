92f47770be242895bc4449bb515a0cae13713a10
PMacald <peter.i.macaldowie@gmail.com>
2018-03-27 12:45:05 +0100

    Changes to population script

--- wad_project/wad_project/settings.py
+++ wad_project/wad_project/settings.py
@@ -33,7 +33,8 @@ SECRET_KEY = '0vdfqb33$k(s&c9sr8=o*ty4)g&89celnp%5rwd*up2q*uc28f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['pmacald.pythonanywhere.com']
+ALLOWED_HOSTS = ['pmacald.pythonanywhere.com',
+                 '127.0.0.1']
 
 # Include url for redirection after login
 
